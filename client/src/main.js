9267a6b872c6ee3d9690b3379e050c70b4a39bd0
JohnyNiks <jonnyniks@gmail.com>
2019-08-16 14:18:10 +0300

    add first page

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,17 @@
 import Vue from "vue";
+
 import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+
+axios.defaults.baseURL = `${process.env.VUE_APP_API_URL}`;
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-col cols="12" sm="6" md="5">
+      <v-card ref="form">
+        <v-card-text>
+          <v-text-field
+            v-model="coupon"
+            label="Coupon"
+            placeholder="####-####-####"
+            outlined
+            clearable
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="mr-4" @click="onSubmit">submit</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-img :src="image" v-show="image" aspect-ratio="1" class="grey lighten-2"></v-img>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  data: () => ({
+    coupon: "",
+    image: ""
+  }),
+  methods: {
+    async onSubmit() {
+      try {
+        const response = await axios.post("/coupon", { body: this.coupon });
+        this.image = `${process.env.VUE_APP_API_URL}${response.data}`;
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+</style>
