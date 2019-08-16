68f6fb4035d610a2bf40a44bb72d9d59c8958ad4
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-08-16 14:04:27 +0300

    add arg

--- server/src/puppeteer.js
+++ server/src/puppeteer.js
@@ -1,7 +1,10 @@
 const puppeteer = require("puppeteer");
 
 export async function processCoupon(coupon) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"]
+  });
   const page = await browser.newPage();
   await page.goto("https://mcdonalds.fast-insight.com/voc/by/ru", {
     waitUntil: "networkidle2"
