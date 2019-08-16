2513a16e60b6493f04eac8af109e991cbf4b9690
Cheslav Zhuravskiy <happylolonly@gmail.com>
2019-08-16 13:11:36 +0300

    add port from process

--- server/src/app.js
+++ server/src/app.js
@@ -24,6 +24,8 @@ app.post("/coupon", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server ready on port:", 3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server ready on port:", PORT);
 });
